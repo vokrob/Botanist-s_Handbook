30a23ec9a1d652428799fbaecd800eccfbee5ab4
vokrob <106639488+vokrob@users.noreply.github.com>
2024-12-18 15:26:25 +0700

    Creating a master list markup, retrieving a list from a category

--- app/src/main/java/com/vokrob/botanists_handbook/MainActivity.kt
+++ app/src/main/java/com/vokrob/botanists_handbook/MainActivity.kt
@@ -1,23 +1,31 @@
 package com.vokrob.botanists_handbook
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.vokrob.botanists_handbook.ui.theme.Botanists_HandbookTheme
 import com.vokrob.botanists_handbook.ui_components.DrawerMenu
+import com.vokrob.botanists_handbook.ui_components.MainListItem
 import com.vokrob.botanists_handbook.ui_components.MainTopBar
 import com.vokrob.botanists_handbook.utils.DrawerEvents
+import com.vokrob.botanists_handbook.utils.IdArrayList
+import com.vokrob.botanists_handbook.utils.ListItem
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -41,6 +49,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             val scaffoldState = rememberScaffoldState()
             val coroutineScope = rememberCoroutineScope()
+            val mainList = remember { mutableStateOf(getListItemsByIndex(0, this)) }
             val topBarTitle = remember { mutableStateOf("Грибы") }
 
             Botanists_HandbookTheme {
@@ -57,15 +66,40 @@ class MainActivity : ComponentActivity() {
                             when (event) {
                                 is DrawerEvents.OnItemClick -> {
                                     topBarTitle.value = event.title
+                                    mainList.value =
+                                        getListItemsByIndex(event.index, this@MainActivity)
                                 }
                             }
                             coroutineScope.launch { scaffoldState.drawerState.close() }
                         }
                     }
-                ) { }
+                ) {
+                    LazyColumn(modifier = Modifier.fillMaxSize()) {
+                        items(mainList.value) { item ->
+                            MainListItem(item = item)
+                        }
+                    }
+                }
             }
         }
     }
+}
+
+private fun getListItemsByIndex(index: Int, context: Context): List<ListItem> {
+    val list = ArrayList<ListItem>()
+    val arrayList = context.resources.getStringArray(IdArrayList.listId[index])
+
+    arrayList.forEach { item ->
+        val itemArray = item.split("|")
+
+        list.add(
+            ListItem(
+                itemArray[0],
+                itemArray[1]
+            )
+        )
+    }
+    return list
 }
 
 
---
+++ app/src/main/java/com/vokrob/botanists_handbook/utils/IdArrayList.kt
@@ -0,0 +1,34 @@
+package com.vokrob.botanists_handbook.utils
+
+import com.vokrob.botanists_handbook.R
+
+object IdArrayList {
+    val listId = listOf(
+        R.array.mushrooms,
+        R.array.medicinal_herbs,
+        R.array.trees,
+        R.array.fruits,
+        R.array.vegetables
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
